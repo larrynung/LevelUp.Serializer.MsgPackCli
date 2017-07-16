15ca7bcb30315219f36b272a327955ce5f1356b0
unknown <larry.nung@TWDT197.xuenn.com>
2017-07-16 15:05:41 +0800

    Add to vcs

---
+++ Source/LevelUp.Serializer.MsgPackCli/Src/Class/Serializer/MsgPackCliSerializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MsgPack.Serialization;
+
+namespace LevelUp.Serializer.MsgPackCli
+{
+    public class MsgPackCliSerializer : SerializerBase
+    {
+        public override void SerializeToStream<T>(T obj, Stream stream, bool enableCompress = false)
+        {
+            var serializer = SerializationContext.Default.GetSerializer<T>();
+            serializer.Pack(stream, obj);
+        }
+
+        public override T DeSerializeFromStream<T>(Stream stream, bool enableDecompress = false)
+        {
+            var serializer = SerializationContext.Default.GetSerializer<T>();
+            return serializer.Unpack(stream);
+        }
+    }
+}
